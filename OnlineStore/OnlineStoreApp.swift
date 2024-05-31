04c5de48e9e9d73c2fd1e87ed9578bf03d6eec1e
Ali Hodroj <aloshito2004@gmail.com>
2024-05-31 23:35:52 +0300

    Initial Commit

---
+++ OnlineStore/OnlineStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineStoreApp.swift
+//  OnlineStore
+//
+//  Created by Ali Hodroj on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnlineStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
